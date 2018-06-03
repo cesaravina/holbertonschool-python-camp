61d2f55b6c81504cdebab53f112cc979c3193b28
Cesar Avina <cesaravina@gmail.com>
2018-06-03 22:58:07 +0000

    alphabits

---
+++ 0x02-python_if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for a in "abcdefghijklmnopqrstuvwxyz":
+    if a == "e":
+        continue
+    elif a == "q":
+        continue
+    else:
+        print("{}".format(a), end='')
