8385eaa082f11882086d2c6617886ef2b518a849
Cesar Avina <cesaravina@gmail.com>
2018-06-03 23:47:35 +0000

    :02d format!

--- 0x02-python_if_else_loops_functions/5-print_comb2.py
+++ 0x02-python_if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range (0, 100):
-    if num < 10:
-        print("0{}, ".format(num), end='')
-    elif num >= 10:
-        print("{}, ".format(num), end='')
+    if num < 99:
+        print("{:02d}, ".format(num), end='')
+    elif num == 99:
+        print("{}".format(num), end='')
