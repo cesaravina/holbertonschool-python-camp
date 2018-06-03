627a9fc640435c6b79cb990f1fd3e80e10c26c7d
Cesar Avina <cesaravina@gmail.com>
2018-06-03 23:57:21 +0000

    leaving!

--- 0x02-python_if_else_loops_functions/5-print_comb2.py
+++ 0x02-python_if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for num in range(0, 100):
-    if num < 99:
-        print("{:02d}, ".format(num), end='')
-    elif num == 99:
-        print("{:02d}".format(num), end='')
+for num in range(0, 99):
+    print("{:02d}, ".format(num), end='')
+print("{:02d}".format(99))
