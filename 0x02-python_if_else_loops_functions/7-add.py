7acd9bc37a761a8fd54f79a8d6b69e3612da906e
Cesar Avina <cesaravina@gmail.com>
2018-06-04 00:59:56 +0000

    not a number?

--- 0x02-python_if_else_loops_functions/7-add.py
+++ 0x02-python_if_else_loops_functions/7-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def add(a, b):
-    return a + b
-
+    if type(a) and type(b) is int:
+        return a + b
+    else:
+        return "not a number"
 print(add(1, 2))
 print(add(98, 0))
 print(add(100, -2))
